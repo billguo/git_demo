92a1289b8a5e821932e85924411982802870bba9
billguo <36311719@qq.com>
2017-04-28 15:06:31 +0800

    首次提交到本地仓库

---
+++ src/git_demo/com/test/helloWorld.java
@@ -0,0 +1,12 @@
+package git_demo.com.test;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("myFirstGitDemo!!!");
+
+	}
+
+}
